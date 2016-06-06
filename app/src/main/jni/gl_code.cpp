d0a008e8fd5e5ec1b3ca296eeb51d502df95d454
Alexey Kuznetsov <axet@me.com>
2016-06-06 16:49:50 +0300

    initial commit

---
+++ app/src/main/jni/gl_code.cpp
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// OpenGL ES 2.0 code
+
+#define NANOVG_GLES2_IMPLEMENTATION
+
+#include <jni.h>
+#include <android/log.h>
+
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <stdio.h>
+#include "nanovg.h"
+#include "nanovg_gl.h"
+#include "nanovg_gl_utils.h"
+
+#define  LOG_TAG    "libgl2jni"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+
+static void printGLString(const char *name, GLenum s) {
+    const char *v = (const char *) glGetString(s);
+    LOGI("GL %s = %s\n", name, v);
+}
+
+static void checkGlError(const char* op) {
+    for (GLint error = glGetError(); error; error
+            = glGetError()) {
+        LOGI("after %s() glError (0x%x)\n", op, error);
+    }
+}
+
+NVGcontext* vg = NULL;
+int w,h;
+
+bool setupGraphics(int w, int h) {
+    printGLString("Version", GL_VERSION);
+    printGLString("Vendor", GL_VENDOR);
+    printGLString("Renderer", GL_RENDERER);
+    printGLString("Extensions", GL_EXTENSIONS);
+
+    LOGI("setupGraphics(%d, %d)", w, h);
+
+    ::w = w;
+    ::h = h;
+
+    glViewport(0, 0, w, h);
+    checkGlError("glViewport");
+
+    vg = nvgCreateGLES2(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
+
+    return true;
+}
+
+void renderFrame() {
+    static float grey = 0.5;
+    glClearColor(grey, grey, grey, 1.0f);
+    checkGlError("glClearColor");
+
+    glClear( GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+    checkGlError("glClear");
+
+    nvgBeginFrame(vg, w, h, 1);
+
+    NVGpaint paint;
+
+    float r1 = (w < h ? w : h) * 0.5f - 5.0f;
+    float r0 = r1 - 20.0f;
+    float r = r0 - 6;
+    float ax = cosf(120.0f/180.0f*NVG_PI) * r;
+    float ay = sinf(120.0f/180.0f*NVG_PI) * r;
+    float bx = cosf(-120.0f/180.0f*NVG_PI) * r;
+    float by = sinf(-120.0f/180.0f*NVG_PI) * r;
+    float hue = sinf(0.5 * 0.12f);
+
+    nvgTranslate(vg, 500,500);
+
+    nvgBeginPath(vg);
+    nvgMoveTo(vg, r,0);
+    nvgLineTo(vg, ax,ay);
+    nvgLineTo(vg, bx,by);
+    nvgClosePath(vg);
+    paint = nvgLinearGradient(vg, r,0, ax,ay, nvgHSLA(hue,1.0f,0.5f,255), nvgRGBA(255,255,255,255));
+    nvgFillPaint(vg, paint);
+    nvgFill(vg);
+    paint = nvgLinearGradient(vg, (r+ax)*0.5f,(0+ay)*0.5f, bx,by, nvgRGBA(0,0,0,0), nvgRGBA(0,0,0,255));
+    nvgFillPaint(vg, paint);
+    nvgFill(vg);
+    nvgStrokeColor(vg, nvgRGBA(0,0,0,64));
+    nvgStroke(vg);
+
+    nvgEndFrame(vg);
+}
+
+extern "C" {
+    JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_init(JNIEnv * env, jobject obj,  jint width, jint height);
+    JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_step(JNIEnv * env, jobject obj);
+};
+
+JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_init(JNIEnv * env, jobject obj,  jint width, jint height)
+{
+    setupGraphics(width, height);
+}
+
+JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_step(JNIEnv * env, jobject obj)
+{
+    renderFrame();
+}
